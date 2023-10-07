368c91d1087085c1e3562adedbebbba99a1bc147
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2023-10-07 08:05:45 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import image from "./assets/progress.jpg";
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="main">
+        <img src={image} alt="website in progress" />
+      </div>
+    </>
+  );
+}
+
+export default App;
