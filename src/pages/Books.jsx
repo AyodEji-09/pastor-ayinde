5cf72a8f37cf60af2867e7d8cfd57edf7f2f0464
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2023-10-16 18:35:24 +0100

    books page ready

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -23,27 +23,22 @@ const Books = () => {
                   />
                   <div className="card-body">
                     <a href={book.url} target="_blank">
-                      <h5 className="card-title text-primary fw-bold">{book.title}</h5>
+                      <h5 className="card-title text-primary fw-bold">
+                        {book.title}
+                      </h5>
                     </a>
                     <p className="card-text">
                       {book.description.substring(0, 100)}...
                     </p>
                     <div className="d-flex gap-1 flex-wrap">
+                      <span>Formats: </span>
                       {book?.format?.map((format, index) => (
                         <a
                           key={index}
                           target="_blank"
                           href={format.url}
-                          className="btn btn-outline-primary"
-                          style={{ padding: "8px" }}
                         >
-                          {format.type} <br />{" "}
-                          <span
-                            style={{ paddingTop: "3px" }}
-                            className="text-secondary"
-                          >
-                            {format.price}
-                          </span>
+                          {format.type}
                         </a>
                       ))}
                     </div>
