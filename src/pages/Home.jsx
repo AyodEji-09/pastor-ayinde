b63957b8b5cf120d004e791b75d09dba2bcd5225
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2023-10-20 02:22:00 +0100

    home page redesign

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,61 +30,50 @@ const Home = () => {
       <main id="hero">
         <div className="container">
           <div className="row d-flex justify-content-center">
-            <div className="col-lg-6">
-              <div className="vh-100 d-flex flex-column justify-content-center align-items-center">
-                {/* <p className="bg-white shadow-md p-1 rounded">
-                  <span className="home__name lead text-radial">
+            <div className="col-lg-6 vh-100 d-flex justify-content-center align-items-center position-relative">
+              <div className="p-1 position-absolute bottom-0 right-0 bg-white shadow-lg rounded mx-1 mb-2">
+                <p>
+                  <span className="home__name text-radial">
                     Samuel Ayinde
                   </span>
-                  <span className="lead fs-4" style={{ lineHeight: 1.55 }}>
+                  <span className="lead" style={{ lineHeight: 1.55 }}>
                     holds a PhD in mechanical engineering from the University of
                     Michigan, Ann Arbor and a Graduate Certificate in counseling
-                    from the RCCGNA Seminary, Greenville, Texas, United States.
-                  </span>
-                </p> */}
-                {/* <p>
-                  <span className="lead fs-4" style={{ lineHeight: 1.55 }}>
-                    He has authored a couple of books including Let the Lord be
-                    Magnified, Attaining Academic Excellence...{" "}
+                    from the RCCGNA Seminary, Greenville...
                     <Link
                       className="text-primary text-decoration-underline"
                       to="/about"
                     >
+                      {" "}
                       Read more
                     </Link>
                   </span>
-                </p> */}
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
-      <div className="d-flex justify-content-center align-items-center">
-        <div className="contact-icons bg-white shadow-lg">
-          <Link
-            className="d-flex align-items-center justify-content-center"
-            to="/books"
-          >
-            {" "}
-            Books
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              style={{ margin: "0 5px" }}
-              viewBox="0 0 32 32"
-            >
-              <path
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M16 7S9 1 2 6v22c7-5 14 0 14 0s7-5 14 0V6c-7-5-14 1-14 1Zm0 0v21"
-              />
-            </svg>
-          </Link>
-          {/* <Link
+                </p>
+                <div className="contact-icons">
+                  <Link
+                    className="d-flex align-items-center justify-content-center"
+                    to="/books"
+                  >
+                    {" "}
+                    Books
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      style={{ margin: "0 5px" }}
+                      viewBox="0 0 32 32"
+                    >
+                      <path
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M16 7S9 1 2 6v22c7-5 14 0 14 0s7-5 14 0V6c-7-5-14 1-14 1Zm0 0v21"
+                      />
+                    </svg>
+                  </Link>
+                  {/* <Link
             className="d-flex align-items-center justify-content-center"
             to="/songs"
           >
@@ -107,34 +96,38 @@ const Home = () => {
               </g>
             </svg>
           </Link> */}
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="mailto:sasamuelayinde@gmail.com"
-          >
-            Contact
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-              focusable="false"
-              width="1.5em"
-              height="1.5em"
-              style={{ margin: "0 5px" }}
-              viewBox="0 0 32 32"
-            >
-              <g
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="3"
-              >
-                <path d="M2 26h28V6H2zM2 6l14 10L30 6" />
-              </g>
-            </svg>
-          </a>
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="mailto:sasamuelayinde@gmail.com"
+                  >
+                    Contact
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                      focusable="false"
+                      width="1.5em"
+                      height="1.5em"
+                      style={{ margin: "0 5px" }}
+                      viewBox="0 0 32 32"
+                    >
+                      <g
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="3"
+                      >
+                        <path d="M2 26h28V6H2zM2 6l14 10L30 6" />
+                      </g>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </main>
     </motion.div>
   );
 };
