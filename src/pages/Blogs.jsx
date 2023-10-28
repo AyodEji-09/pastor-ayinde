7099fb5bde5d7456283cb43affc589ad64966cbf
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2023-10-28 07:39:27 +0100

    added social media accounts

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,12 +1,21 @@
 import Breadcrump from "../components/common/Breadcrump";
 import transition from "../components/transition";
 
-
 const Blogs = () => {
   return (
     <section id="blogs">
       <div className="container vh-100">
         <Breadcrump page="Blogs" />
+
+        <div className="row d-flex justify-content-center">
+          <div className="col-lg-6">
+            <img
+              className="img-fluid"
+              src="https://img.freepik.com/free-vector/abstract-grunge-style-coming-soon-with-black-splatter_1017-26690.jpg?w=996&t=st=1698445162~exp=1698445762~hmac=c8ecd293dd95b7ee94e1a0d7f18321cc44493413ee405875e1184b09ba7d8fd7"
+              alt=""
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -8,44 +8,44 @@ const Books = () => {
       <div className="container">
         <Breadcrump page="Books" />
 
-        <div className="row">
-          {books.map((book) => (
-            <>
-              <div
-                key={book.title}
-                className="col-lg-3 col-md-6 col-sm-6 mt-1 d-flex align-items-stretch"
-              >
-                <div className="card">
-                  <img
-                    src={book.img_url}
-                    className="card-img-top"
-                    alt={book.title}
-                  />
-                  <div className="card-body">
-                    <a href={book.url} target="_blank">
-                      <h5 className="card-title text-primary fw-bold">
-                        {book.title}
-                      </h5>
-                    </a>
-                    <p className="card-text">
-                      {book.description.substring(0, 100)}...
-                    </p>
-                    <div className="d-flex gap-1 flex-wrap">
-                      <span>Formats: </span>
-                      {book?.format?.map((format, index) => (
-                        <a
-                          key={index}
-                          target="_blank"
-                          href={format.url}
-                        >
-                          {format.type}
-                        </a>
-                      ))}
-                    </div>
+        <div className="row my-3">
+          {books.map((book, index) => (
+            <div
+              key={book.title + index}
+              className="col-lg-3 col-md-6 col-sm-6 mb-2 d-flex align-items-stretch"
+            >
+              <div className="card">
+                <img
+                  src={book.img_url}
+                  className="card-img-top"
+                  alt={book.title}
+                />
+                <div className="card-body">
+                  <small>{book.dop}</small>
+                  <a href={book.url} target="_blank">
+                    <h5 className="card-title text-primary fw-bold">
+                      {book.title}
+                    </h5>
+                  </a>
+                  <p className="card-text">
+                    {book.description.substring(0, 100)}...
+                  </p>
+                  <div className="d-flex gap-1 flex-wrap">
+                    <span>Formats: </span>
+                    {book?.format?.map((format, index) => (
+                      <a key={index} target="_blank" href={format.url}>
+                        {format.type}
+                      </a>
+                    ))}
+                  </div>
+                  <hr />
+                  <div className="d-flex gap-1 flex-wrap">
+                    <span>Pages: </span>
+                    <em className="fst-italic">{book.pages}</em>
                   </div>
                 </div>
               </div>
-            </>
+            </div>
           ))}
         </div>
       </div>
